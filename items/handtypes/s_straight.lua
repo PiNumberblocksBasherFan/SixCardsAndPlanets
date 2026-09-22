4662c326b76ab69d6975628b1bd835b3a7cdc5c2
PiNumberblocksBasherFan <lockseedden@gmail.com>
2026-09-22 15:14:25 +0800

    Update s_straight.lua to include variants

--- items/handtypes/s_straight.lua
+++ items/handtypes/s_straight.lua
@@ -23,4 +23,26 @@ SMODS.PokerHand {
     evaluate = function(parts, hand)
         return next(parts.mxms_s_straight) and parts.mxms_s_straight or {}
     end
+    modify_display_text = function(self, cards, scoring_hand)
+        local royal = true
+        for i = 1, #scoring_hand do
+            local rank = SMODS.Ranks[scoring_hand[i].base.value]
+            royal = royal and (rank.key == 'Ace' or rank.key == '10' or rank.key == '9' or rank.face)
+            mid = mid and (rank.key == '10' or rank.key == '9' or rank.key == '8' or rank.key == '7' or rank.key == '6' or rank.key == '5')
+            wheel = wheel and (rank.key == 'Ace' orrank.key == '2' or rank.key == '3' or rank.key == '4' or rank.key == '5' or rank.key == '6')
+        end
+        if royal then
+          return 'mxms_catwalk'
+        else
+            if mid then
+              return 'mxms_indispensable'
+            else
+                if wheel then
+                  return 'mxms_ship'
+                else
+                    return 's_straight'
+                end
+            end
+        end
+    end
 }
