fe8f78a4e572aabfdd556855845664ba391e6fe9
PiNumberblocksBasherFan <lockseedden@gmail.com>
2026-09-23 08:37:21 +0800

    Update f_6oak.lua

--- items/handtypes/f_6oak.lua
+++ items/handtypes/f_6oak.lua
@@ -24,4 +24,16 @@ SMODS.PokerHand {
         return next(parts.mxms_6) and next(parts.mxms_s_flush)
             and { SMODS.merge_lists(parts.mxms_6, parts.mxms_s_flush) } or {}
     end
+    modify_display_text = function(self, cards, scoring_hand)
+        local devil = true
+        for i = 1, #scoring_hand do
+            local rank = SMODS.Ranks[scoring_hand[i].base.value]
+            devil = devil and (rank.key == '6')
+        end
+        if devil then
+          return 'mxms_angels_horns'
+        else
+            return 'mxms_f_6oak'
+        end
+    end
 }
